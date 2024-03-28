78b19baf598e8fd9237ce01dbafca7ec0c6aed81
Fernando <fernandocatalamunyoz@gmail.com>
2024-03-28 11:05:59 +0100

    feat: muestro los usuarios registrados en la vista suepr_admin

---
+++ src/common/List/List.jsx
@@ -0,0 +1,12 @@
+import "./List.css";
+
+export const List = ({ firstName, lastName, email, createdAt, deleteUser }) => {
+  return (
+    <div className="listDesign">
+      <div className="userData">{firstName}</div>
+      <div className="userData"> {lastName}</div>
+      <div className="userData">{email}</div>
+      <div className="userData">{createdAt}</div>
+    </div>
+  );
+};
--- src/pages/SuperAdmin/SuperAdmin.jsx
+++ src/pages/SuperAdmin/SuperAdmin.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { GetUsers } from "../../services/apiCalls";
 import { Header } from "../../common/Header/Header";
 import { Card } from "../../common/Card/Card";
+import "./SuperAdmin.css";
+import { List } from "../../common/List/List";
 
 export const SuperAdmin = () => {
   const user = JSON.parse(localStorage.getItem("passport"));
@@ -29,7 +31,27 @@ export const SuperAdmin = () => {
   return (
     <>
       <Header />
-      <div>Soy la vista superAdmin</div>
+      <div className="usersContainer">
+        <div className="dataHeader">
+          <div className="title">Nombre</div>
+          <div className="title">Apellido</div>
+          <div className="title">email</div>
+          <div className="title">Creado</div>
+        </div>
+        <div className="sAdminDesign">
+          {users.slice(0, 10).map((person) => {
+            return (
+              <List
+                key={person.id}
+                firstName={person.firstName}
+                lastName={person.lastName}
+                email={person.email}
+                createdAt={person.createdAt}
+              ></List>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
